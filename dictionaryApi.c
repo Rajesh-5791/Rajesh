5932d5f7e7462d18de1b4741f1d7f93c20f5b417
Rajesh <158807322+Rajesh-5791@users.noreply.github.com>
2024-02-05 10:19:29 +0530

    Update dictionaryApi.c

--- dictionaryApi.c
+++ dictionaryApi.c
@@ -1,4 +1,4 @@
-// To get meaning of any word using dictionary API
+// To get meaning of any word using dictionary API and to get it's pronunciation
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -18,31 +18,39 @@ char* getAudioUrl(char *);
 
 void main()
 {
-	char url[MAXURLLENGTH];
+    char url[MAXURLLENGTH];
+    while (1)
+    {
+	printf("Enter '0' to exit.\n");
 	printf("Enter a word to get it's meaning: ");
 	scanf("%s", word);
-
+	if (strcmp(word, "0") == 0)
+	{
+		printf("You chose exit. Thank you.\n");
+		exit(0);
+	}
+	
 	sprintf(url, "%s%s%s", "curl -s \"https://api.dictionaryapi.dev/api/v2/entries/en/", word, "\" > dictionaryApiResponse.txt");
 	system(url);
-
+	
 	fpDictionary = fopen("dictionaryApiResponse.txt", "r");
 	fseek(fpDictionary, 0, SEEK_END);
 	fileSize = ftell(fpDictionary);
 	fseek(fpDictionary, 0, SEEK_SET);
 	char dictionaryApiResponse[fileSize];
 	fgets(dictionaryApiResponse, fileSize, fpDictionary);
-
+	
 	definition = malloc(MAXDEFINITIONLENGTH);
 	definition = getDefinition(dictionaryApiResponse);
 	if (strcmp(definition, "Not found") == 0)
 	{
-		printf("%s not found in the dictionary.\n", word);
+		printf("%s word not found in the dictionary.\n", word);
 	}
 	else
 	{
 		printf("\nDefinition of %s: %s\n", word, definition);
 	}
-
+	
 	fseek(fpDictionary, 0, SEEK_SET);
 	fgets(dictionaryApiResponse, fileSize, fpDictionary);
 	audioUrl = malloc(MAXAUDIOURLLENGTH);
@@ -56,12 +64,13 @@ void main()
 		playPronunciation(audioUrl);
 	}
 	fclose(fpDictionary);
+    }
 }
 
 char* getAudioUrl(char *dictionaryApiResponse)
 {
-	int isAudioFound = 0;
-	strcpy(audioUrl, "Not found");
+    int isAudioFound = 0;
+    strcpy(audioUrl, "Not found");
 
     char *token = strtok(dictionaryApiResponse, "\"");
     while (token != NULL) 
@@ -71,34 +80,24 @@ char* getAudioUrl(char *dictionaryApiResponse)
             token = strtok(NULL, "\"");
             token = strtok(NULL, "\"");
             strcpy(audioUrl, token);
-            if (strlen(audioUrl) > 10) 
+            if (strlen(audioUrl) < 10) 
             {
-                break;
+	        token = strtok(NULL, "\"");
+	        isAudioFound = 1;
+                continue;
             } 
             else 
             {
-	            isAudioFound = 1;
-            	while (token != NULL)
-            	{	
-	                if (strcmp(token, "audio") == 0) 
-		            {
-		                token = strtok(NULL, "\"");
-		                token = strtok(NULL, "\"");
-			            strcpy(audioUrl, token);		                
-		                isAudioFound = 0;
-		                break;
-		            }	            
-			        token = strtok(NULL, "\"");
-            	}
+	        isAudioFound = 0;
+	        break;
             }
-            break;
         }
         token = strtok(NULL, "\"");
     }
     if (isAudioFound == 1)
     {
     	fseek(fpDictionary, 0, SEEK_SET);
-		fgets(dictionaryApiResponse, fileSize, fpDictionary);
+	fgets(dictionaryApiResponse, fileSize, fpDictionary);
         char *token = strtok(dictionaryApiResponse, ":");
         while (token != NULL) 
         {
@@ -116,8 +115,8 @@ char* getAudioUrl(char *dictionaryApiResponse)
 
 char* getDefinition(char *dictionaryApiResponse)
 {
-	strcpy(definition, "Not found");
-	char *token = (char *)strtok(dictionaryApiResponse, ":");
+    strcpy(definition, "Not found");
+    char *token = (char *)strtok(dictionaryApiResponse, ":");
     while (token != NULL) 
     {
         if (strcmp(token, "[{\"definition\"") == 0) 
@@ -133,7 +132,7 @@ char* getDefinition(char *dictionaryApiResponse)
 
 void playPronunciation(char *audioUrl)
 {
-    printf("\nPlaying %s pronunciation...\n", word);
+    printf("\nPlaying %s pronunciation...\n\n", word);
     char playCommand[MAXURLLENGTH];
     sprintf(playCommand, "start %s", audioUrl);
     system(playCommand);
